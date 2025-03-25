e7cfb615e72a2f4f09d84d175fd6887569d529d3
yjc0901 <yayaz96@naver.com>
2025-03-25 11:27:15 +0900

    가격 추가, 문의사항 오류 수정
    Signed-off-by: yjc0901 <yayaz96@naver.com>

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // === MySQL 연결 설정 ===
 // const _pool = mysql2.createPool({
